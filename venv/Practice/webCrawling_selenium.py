0e9433f9b22a4bb9dbd54e22b8dfab5c50dc0f46
kthss01 <kthss01@naver.com>
2020-11-28 17:02:35 +0900

    1일차 스트라바 파이썬 웹크롤링 연습

---
+++ venv/Practice/webCrawling_selenium.py
@@ -0,0 +1,32 @@
+# 절대경로 가져오기
+import os.path
+path = os.path.abspath("../chromedriver")
+# print(path)
+
+# 자동화 테스트를 위해 셀레니움(Selenium) 불러오기
+from selenium import webdriver
+from time import sleep
+
+# 크롬 웹드라이버 경로 설정
+driver = webdriver.Chrome(path)
+
+# 크롬을 통해 구글 로그인 화면에 접속
+loginUrl = 'https://www.strava.com/login'
+driver.get(loginUrl)
+
+sleep(0.5)
+driver.find_element_by_xpath('//*[@id="login_form"]/div[2]/a').click()
+
+# 로그인 아이디 입력
+sleep(0.5)
+id = 'kthtim0704@gmail.com'
+driver.find_element_by_name('identifier').send_keys(id)
+
+sleep(0.5)
+driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
+
+# 비밀번호 입력 받기
+import getpass
+
+password = input('비밀번호 입력: ')
+
